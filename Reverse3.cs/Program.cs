bad0949ce2cb82912e5b64f99cac7c6e3c2bf92e
RMiller28 <Millerrycki5@gmail.com>
2014-05-09 10:05:42 -0400

    New Change

---
+++ Reverse3.cs/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Reverse3
+{
+    class Program
+    {
+        public static int Revthat(ref int a, ref int b, ref int c)
+        {
+
+
+        }
+
+        static void Main(string[] args)
+        {
+            int[] fi = 2, si = 1, ti = 1;
+            Revthat(ref fi, ref si, ref ti);
+        }
+    }
+}
