54a7f04aabbab474a94581aeb5a1b7c0e5ac089d
RMiller28 <Millerrycki5@gmail.com>
2014-05-14 09:04:09 -0400

    Trying

--- Reverse4.cs/Program.cs
+++ Reverse4.cs/Program.cs
@@ -5,18 +5,10 @@ using System.Text;
 
 namespace Reverse4.cs
 {
-    class Reverse4
+    class Program
     {
-        public static void Reverse(int a, int b, int c, int d)
-        {
-            Console.WriteLine("Reversed: {0}, {1}, {2}, {3}", a, b, c, d);
-        }
         static void Main(string[] args)
         {
-            int firstInt = 10, secondInt = 20, thirdInt = 30, fourthInt = 40;
-            Console.WriteLine("In Order: {0}, {1}, {2} {3}", firstInt, secondInt, thirdInt, fourthInt);
-            Reverse(fourthInt, thirdInt, secondInt, firstInt);
-
         }
     }
 }
