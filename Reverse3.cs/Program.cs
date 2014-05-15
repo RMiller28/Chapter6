3b3f4056d622c16b425507d60390235b674cb252
RMiller28 <Millerrycki5@gmail.com>
2014-05-15 10:09:26 -0400

    almost

--- Reverse3.cs/Program.cs
+++ Reverse3.cs/Program.cs
@@ -7,8 +7,20 @@ namespace Reverse3.cs
 {
     class Program
     {
+
+        public static void reverse(ref int a, ref int b, ref int c)
+        {
+            int temp = a;
+            a = c;
+            c = temp;
+        }
+
         static void Main(string[] args)
         {
+            int x = 1, y = 2, z = 3;
+
+            reverse(ref x, ref y, ref z);
+
         }
     }
 }
--- Reverse4.cs/Program.cs
+++ Reverse4.cs/Program.cs
@@ -7,8 +7,20 @@ namespace Reverse4.cs
 {
     class Program
     {
+
+        public static void reverse(ref int a, ref int b, ref int c, ref int d)
+        {
+            int temp = a;
+            a = c;
+            c = temp;
+        }
+
         static void Main(string[] args)
         {
+            int w = 1, x = 2, y = 3, z = 4;
+
+            reverse(ref w, ref x, ref y, ref z);
+
         }
     }
 }
